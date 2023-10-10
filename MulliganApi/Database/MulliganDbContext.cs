b8546a84c2c6fc30cb728cfdee427b22aaf15324
Edvardunsvag <54942158+Edvardunsvag@users.noreply.github.com>
2023-10-10 21:29:24 +0200

    connection string

--- MulliganApi/Database/MulliganDbContext.cs
+++ MulliganApi/Database/MulliganDbContext.cs
@@ -17,7 +17,7 @@ namespace MulliganApi.Data
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.
-                UseSqlServer(_configuration.GetConnectionString("TestDb"));
+                UseSqlServer("Server=tcp:mulligan-dbserver-test.database.windows.net,1433;Initial Catalog=mulligan-db-test;Persist Security Info=False;User ID=mulligan-login;Password=HoleInOne1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
         }
 
         public DbSet<User> User { get; set; }
