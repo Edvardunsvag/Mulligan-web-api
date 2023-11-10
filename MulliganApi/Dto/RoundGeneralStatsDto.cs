067b73e50894a5fea77f21fc440e8cb6ab6bde48
Edvard Unsvåg <edvardunsvag@Edvards-MacBook-Pro.local>
2023-11-10 22:08:56 +0100

    code

--- MulliganApi/Dto/RoundGeneralStatsDto.cs
+++ MulliganApi/Dto/RoundGeneralStatsDto.cs
@@ -3,6 +3,6 @@ namespace MulliganApi.Dto;
 public class HoleGeneralStats
 {
     public string ScoreName { get; set; }
-    public double? Percentage { get; set; }
+    public double Percentage { get; set; } = 0.0;
     public int ScoreAmount { get; set; }
 }
